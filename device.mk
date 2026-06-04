50ce43836f31a0118b6bd542fda4f11d021d57c1
HYCQAQ <75055152+HYCQAQ@users.noreply.github.com>
2026-06-05 00:49:40 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/arbor/GT78-VN
+LOCAL_PATH := device/arbor/GT78_VN
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
