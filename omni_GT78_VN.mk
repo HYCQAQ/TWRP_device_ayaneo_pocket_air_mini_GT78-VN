2c885eeaad2f0e37d25cdaffb39cb1cda70de5ab
HYCQAQ <75055152+HYCQAQ@users.noreply.github.com>
2026-06-04 20:51:28 +0800

    Update omni_GT78_VN.mk

--- omni_GT78_VN.mk
+++ omni_GT78_VN.mk
@@ -6,7 +6,7 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+# $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit some common Omni stuff.
