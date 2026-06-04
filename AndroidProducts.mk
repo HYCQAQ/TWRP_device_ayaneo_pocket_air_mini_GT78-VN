91b76e223b8473001502ce7e73066aaa571d8531
HYCQAQ <75055152+HYCQAQ@users.noreply.github.com>
2026-06-05 07:54:13 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,3 +4,11 @@ PRODUCT_MAKEFILES := \
 COMMON_LUNCH_CHOICES := \
     twrp_GT78_VN-userdebug \
     twrp_GT78_VN-eng
+
+# 必须写在 AndroidBoard.mk 或 Android.mk 中
+$(PRODUCT_OUT)/dtb.img: $(LOCAL_PATH)/prebuilt/dtb.img
+	@echo "==== 正在强行搬运预编译 DTB 镜像 ===="
+	$(hide) mkdir -p $(dir $@)
+	$(hide) cp $< $@
+
+.PHONY: $(PRODUCT_OUT)/dtb.img
