83016f64e7108002eff3a8555ceccd72283a0146
HYCQAQ <75055152+HYCQAQ@users.noreply.github.com>
2026-06-04 17:35:00 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -9,6 +9,5 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_GT78-VN.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_GT78-VN-user \
-    omni_GT78-VN-userdebug \
-    omni_GT78-VN-eng
+    omni_GT78_VN-userdebug \
+    omni_GT78_VN-eng
