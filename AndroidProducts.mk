f6ae7fbd415a523a9daee03673be2e5b8b573ee3
HYCQAQ <75055152+HYCQAQ@users.noreply.github.com>
2026-06-04 23:08:32 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,6 @@
-#
-# Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_GT78_VN.mk
+    $(LOCAL_DIR)/twrp_GT78_VN.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_GT78_VN-userdebug \
-    omni_GT78_VN-eng
+    twrp_GT78_VN-userdebug \
+    twrp_GT78_VN-eng
