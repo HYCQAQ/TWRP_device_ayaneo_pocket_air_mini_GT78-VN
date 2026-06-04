3b584b58e902ed3b847a13af6f865a6d2e8524de
HYCQAQ <75055152+HYCQAQ@users.noreply.github.com>
2026-06-04 19:46:32 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -17,15 +17,8 @@ AB_OTA_POSTINSTALL_CONFIG += \
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
     android.hardware.boot@1.0-service
-
-PRODUCT_PACKAGES += \
     bootctrl.mt6785
 
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.mt6785 \
-    libgptutils \
-    libz \
-    libcutils
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
